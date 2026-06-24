7095dc5296467449cce7e3633654f50e164882b1
SabnamJuhi <sjuhisarkars95@gmail.com>
2026-06-24 17:04:36 +0530

    first commitdhhdhjghwjdhbhdfd

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -13,7 +13,7 @@
 
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL,, {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   withCredentials: true,
     autoConnect: false,
   //  transports: ["websocket", "polling"],
